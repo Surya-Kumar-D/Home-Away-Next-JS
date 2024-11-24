bd5e6f50a835bbba3a5a01a36ae93907ac456e4e
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-11-24 20:17:58 +0530

    - create common container class for the page to cotain and center the page

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,5 +1,21 @@
+import DarkMode from "./DarkMode";
+import LinksDropDown from "./LinksDropDown";
+import Logo from "./Logo";
+import NavSearch from "./NavSearch";
+
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav className="border-b">
+      <div className="container flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-4 py-8">
+        <Logo />
+        <NavSearch />
+        <div className="flex gap-4 items-center ">
+          <DarkMode />
+          <LinksDropDown />
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
