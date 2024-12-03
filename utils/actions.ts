8215890136c0df01e65455c41c44fa6aa498579f
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-03 13:29:49 +0530

    add favorite toggle form to display favorite cards in the favorite page

--- utils/actions.ts
+++ utils/actions.ts
@@ -238,3 +238,25 @@ export const toggleFavoriteAction = async (prevState: {
     return renderError(error);
   }
 };
+
+export const fetchFavorites = async () => {
+  const user = await getAuthUser();
+  const favorites = await prisma.favorite.findMany({
+    where: {
+      profileId: user.id,
+    },
+    select: {
+      property: {
+        select: {
+          id: true,
+          name: true,
+          tagline: true,
+          country: true,
+          price: true,
+          image: true,
+        },
+      },
+    },
+  });
+  return favorites.map((favorite) => favorite.property);
+};
