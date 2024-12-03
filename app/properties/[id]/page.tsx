db1fecef081d89ff7778ae992232ba675d5f12b1
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-03 20:41:42 +0530

    create calendar with react-day picker create userinfo component for properties page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,8 +1,11 @@
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import PropertyRating from "@/components/card/PropertyRating";
+import BookingCalendar from "@/components/properties/BookingCalendar";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import ImageContainer from "@/components/properties/ImageContainer";
+import PropertyDetails from "@/components/properties/PropertyDetails";
 import ShareButton from "@/components/properties/ShareButton";
+import UserInfo from "@/components/properties/UserInfo";
 import { fetchPropertyDetails } from "@/utils/actions";
 import { redirect } from "next/navigation";
 import React from "react";
@@ -12,7 +15,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(id)!;
   const { beds, bedrooms, guests, baths, image, name } = property!;
   const details = { beds, baths, bedrooms, guests };
-
+  const { firstName, profileImage } = property?.profile!;
   if (!params) redirect("/");
   return (
     <div>
@@ -25,15 +28,17 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
         </div>
       </header>
       <ImageContainer mainImage={image} name={name} />
+      <UserInfo profile={{ firstName, profileImage }} />
       <section className="lg:grid lg:grid-cols-12 gap-x-12 mt-12">
         <div className="lg:col-span-8">
           <div className="flex gap-x-4 items-center">
-            <h1 className="text-xl font-bold">{name}</h1>
+            <h1 className="text-xl capitalize font-bold">{name}</h1>
             <PropertyRating inPage propertyId={id} />
           </div>
+          <PropertyDetails details={details} />
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
-          {/* {Calender} */}
+          <BookingCalendar />
         </div>
       </section>
     </div>
